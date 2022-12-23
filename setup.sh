d71e0e084f8673dd289f6d3c1d8a860991f09b22
Lushay Labs <107283894+lushaylabs@users.noreply.github.com>
2022-12-23 12:00:25 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+USERNAME=$USER
+
+echo "Giving User USB Permission for openFPGALoader"
+
+wget https://raw.githubusercontent.com/trabucayre/openFPGALoader/master/99-openfpgaloader.rules -o /tmp/99-openfpgaloader.rules
+sudo -s <<EOF
+cp /tmp/99-openfpgaloader.rules /etc/udev/rules.d/
+udevadm control --reload-rules && sudo udevadm trigger
+usermod -a $USERNAME -G plugdev
+EOF
+
+echo "Done, Unplug and replug in the device now"
